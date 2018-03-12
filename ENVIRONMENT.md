b4c6cb8b69a5d34ae184ea62ee0080c92d7e36b3
Rockie Yang <eyouyan@gmail.com>
2018-03-12 09:44:44 +0100

    SEO, Supervised-Learning-Explained-2

---
+++ ENVIRONMENT.md
@@ -0,0 +1,8 @@
+
+
+Instructions:
+
+- Install Docker
+- Run ./jupyter.sh
+- Open browser with localhost:8888
+- CTRL + C, ./jupyter.sh, when no longer needed. No trash will be left
--- jupyter.sh
+++ jupyter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm -p 8000:8888 -v `pwd`:/opt/knockdata knockdata/python3 jupyter notebook --notebook-dir=/opt/knockdata --ip='*' --port=8888 --no-browser --allow-root --NotebookApp.token=''
+docker run -it --rm -p 8888:8888 -v `pwd`:/opt/knockdata knockdata/python3 jupyter notebook --notebook-dir=/opt/knockdata --ip='*' --port=8888 --no-browser --allow-root --NotebookApp.token=''
